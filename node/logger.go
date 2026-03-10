d10d685a4eaeada507725d03c37ccb0c4facd36a
Tee <knapstoremail@gmail.com>
2026-03-10 08:10:00 +0100

    chore: add zerolog dependency for structured JSON logging

---
+++ node/logger.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+var logger zerolog.Logger
+var auditLogger zerolog.Logger
+
+// InitLogger sets up the structured JSON logger and a daily audit log file.
+func InitLogger(level string) {
+	lvl := zerolog.InfoLevel
+	switch strings.ToLower(level) {
+	case "debug":
+		lvl = zerolog.DebugLevel
+	case "warn":
+		lvl = zerolog.WarnLevel
+	case "error":
+		lvl = zerolog.ErrorLevel
+	}
+	zerolog.SetGlobalLevel(lvl)
+	logger = zerolog.New(os.Stdout).With().Timestamp().Str("service", "soroban-encrypt-node").Logger()
+
+	af, err := openAuditFile()
+	var w io.Writer = os.Stdout
+	if err == nil {
+		w = io.MultiWriter(os.Stdout, af)
+	}
+	auditLogger = zerolog.New(w).With().Timestamp().Str("component", "audit").Logger()
+}
+
+func openAuditFile() (*os.File, error) {
+	dir := os.Getenv("DATA_DIR")
+	if dir == "" {
+		dir = "./data"
+	}
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return nil, err
+	}
+	date := time.Now().Format("2006-01-02")
+	return os.OpenFile(dir+"/audit-"+date+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+}
+
+// maskBytes redacts sensitive byte slices in log fields.
+func maskBytes(b []byte) string {
+	if len(b) <= 4 {
+		return "****"
+	}
+	return "****[truncated]"
+}
